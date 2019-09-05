3e72f14a2d2dc1fbcb5fbd39e587e1930a262749
Michael Aaron Murphy <mmstick@pm.me>
2019-09-05 11:58:50 -0600

    fix(examples): Fix the upgrade example

--- examples/upgrade.rs
+++ examples/upgrade.rs
@@ -1,10 +1,11 @@
 extern crate apt_sources_lists;
 
 use apt_sources_lists::*;
+use std::collections::HashSet;
 
 pub fn main() {
     let mut list = SourcesLists::scan().unwrap();
-    match list.dist_upgrade("disco", "cosmic") {
+    match list.dist_upgrade(&HashSet::new(), "disco", "cosmic") {
         Ok(()) => println!("successfully upgraded"),
         Err(why) => eprintln!("failed to upgrade: {}", why),
     }
